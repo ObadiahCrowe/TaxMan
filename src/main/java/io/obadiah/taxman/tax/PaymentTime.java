4f88c14241d4292180f0543933f67984585e4aed
Obadiah Crowe <me@obadiah.io>
2021-11-02 02:18:13 +0000

    More commenting

--- src/main/java/io/obadiah/taxman/tax/PaymentTime.java
+++ src/main/java/io/obadiah/taxman/tax/PaymentTime.java
@@ -1,5 +1,8 @@
 package io.obadiah.taxman.tax;
 
+/**
+ * Represents the days of the month to attempt to automatically pay an individual's income tax.
+ */
 public enum PaymentTime {
 
     START_OF_MONTH(1, 3, 5),
@@ -8,10 +11,18 @@ public enum PaymentTime {
 
     private final int[] attemptDays;
 
+    /**
+     * Represents a period within a month to attempt to automatically pay and individual's income tax.
+     *
+     * @param attemptDays The days to attempt per month.
+     */
     PaymentTime(int... attemptDays) {
         this.attemptDays = attemptDays;
     }
 
+    /**
+     * @return The days to attempt per month.
+     */
     public int[] getAttemptDays() {
         return this.attemptDays;
     }
